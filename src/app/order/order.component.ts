cad3137a63d4111919c8d2acb26d65e44a8b80ee
SebastianStaszalek <sebastian.staszalek@gmail.com>
2018-09-10 22:15:48 +0200

    saves order in db with dishes id, but without counter

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Subscription} from 'rxjs';
 import {Order} from '../model/order.model';
 import {OrderService} from '../order.service';
-import {DishesService} from '../dishes.service';
 import {OrderBasketService} from '../order-basket.service';
+import {BasketDish} from '../model/basket-dish.model';
+import {OrderQuantity} from '../model/order-quantity';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
 
   sub: Subscription;
   order: Order;
   dishIds: number[];
+  basketPositions: BasketDish[];
 
   clientDetails = new FormGroup({
     firstName: new FormControl('', Validators.required),
@@ -37,10 +39,19 @@ export class OrderComponent {
     console.log(this.clientDetails.value);
   }
 
-  addOrder() {
+  ngOnInit(): void {
+    this.getDishes();
+  }
+
+  addOrder(): void {
     this.order = this.clientDetails.value;
     this.order.dishIds = this.dishIds;
     this.orderService.addOrder(this.order).subscribe();
+  }
+
+  getDishes(): void {
+    this.basketPositions = this.basketService.getBasketPositions();
+    this.basketPositions.forEach(dish => this.dishIds.push(dish.id));
   }
 
 }
