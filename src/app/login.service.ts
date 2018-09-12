28ef1ed054fd185fd316a11beb0f4111f521f2ad
SebastianStaszalek <sebastian.staszalek@gmail.com>
2018-09-12 12:39:51 +0200

    login service

---
+++ src/app/login.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {User} from './model/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  isLogged: boolean;
+
+  constructor(
+    private readonly http: HttpClient,
+    private readonly router: Router
+  ) {
+    this.isLogged = false;
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>('/api/users');
+  }
+
+  logIn() {
+    this.isLogged = true;
+    this.router.navigate(['/dashboard-dishes-list']);
+  }
+
+  logOut() {
+    this.isLogged = false;
+    this.router.navigate(['/']);
+  }
+}
